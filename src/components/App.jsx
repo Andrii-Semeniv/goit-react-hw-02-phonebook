18020a0eff688f6b601a05caed5bbcdc8cef6fd0
Andrii-Semeniv <andrii.semeniv7@gmail.com>
2023-04-17 22:29:58 +0300

    startrRefactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
 class App extends Component {
-  state = { contacts: [], name: '' };
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
 
   handleChange = e => {
-    this.setState({ name: e.target.value });
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   };
 
   handleSubmit = e => {
     e.preventDefault();
+
     this.setState(prevState => {
+      const { name, number } = prevState;
       return {
-        contacts: [...prevState.contacts, prevState.name],
+        contacts: [...prevState.contacts, { id: nanoid(), name, number }],
         name: '',
+        number: '',
       };
     });
   };
 
+  filterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
   render() {
     return (
       <>
@@ -36,16 +53,43 @@ class App extends Component {
                 required
               />
             </label>
+            <label>
+              Number
+              <input
+                type="tel"
+                name="number"
+                value={this.state.number}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                onChange={this.handleChange}
+                required
+              />
+            </label>
             <button type="submit">Add contact</button>
           </form>
         </div>
 
         <div>
           <h2>Contacts</h2>
+          <label>
+            Find contacts by name
+            <input
+              type="text"
+              value={this.state.filter}
+              onChange={this.filterChange}
+            />
+          </label>
           <ul>
-            {this.state.contacts.map(contact => (
-              <li key={nanoid()}> {contact}</li>
-            ))}
+            {this.state.contacts.map(
+              ({ id, name, number }) =>
+                name
+                  .toLowerCase()
+                  .includes(this.state.filter.toLowerCase()) && (
+                  <li key={id}>
+                    {name}: {number}
+                  </li>
+                )
+            )}
           </ul>
         </div>
       </>
